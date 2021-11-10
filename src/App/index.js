8cb8bcf5803aff263d9d9409da775d30a1462afb
Michael Tobias <michaelremitobias@gmail.com>
2021-11-09 21:53:14 -0700

    refactored nav to be correct

--- src/App/index.js
+++ src/App/index.js
@@ -1,41 +1,18 @@
 import React from "react";
 import Home from "../Home/index";
-import Products from "../Products/index";
 import ContactUs from "../ContactUs/index";
 import Watches from "../Watches/index";
 import About from "../About/index";
-import Diamonds from "../Diamonds/index";
 import Jewlery from "../Jewlery/index";
-import BeltsAndBuckels from "../BeltsAndBuckels/index";
-import Bracelets from "../Bracelets/index";
-import GoldLink from "../GoldLink/index";
-import FormSuccessful from "../ContactUs/components/FormSuccessful";
-import styled from "styled-components";
 
-import { Nav, NavDropdown, Navbar } from "react-bootstrap";
+import FormSuccessful from "../ContactUs/components/FormSuccessful";
+
+import { Nav, Navbar } from "react-bootstrap";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import LeatherGoods from "../LeatherGoods";
-import PartsAndAccessories from "../PartsAndAccessories";
 
-const Body = styled.div`
-  background-color: #f5f2e9;
-`;
-
-const Header = styled.header`
-  display: flex;
-  justify-context: center;
-  flex-direction: column;
-`;
-
-const Title = styled.h1`
-  color: white;
-`;
-
-const TitleBox = styled.div`
-  background-color: #343a40;
-`;
+import { Body, Header, Title, TitleBox } from "./style.js";
 function App() {
   return (
     <Header>
@@ -66,54 +43,20 @@ function App() {
                 <Nav.Link>
                   <Link to="/contact">Contact Us</Link>
                 </Nav.Link>
-                <NavDropdown title="Watches" id="basic-nav-dropdown">
-                  <NavDropdown.Item>
-                    <Link to="/watches">Pre-Owned Watches</Link>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to="/partsandaccessories">Parts & Accessories</Link>
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title="Jewlery" id="basic-nav-dropdown">
-                  <NavDropdown.Item>
-                    <Link to="/diamonds">Diamonds</Link>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to="/bracelets">Bracelets</Link>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to="/goldlink">Gold Link</Link>
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item>
-                    <Link to="/jewlery">See All Jewlery</Link>
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title="Fine Goods" id="basic-nav-dropdown">
-                  <NavDropdown.Item>
-                    <Link to="/beltsandbuckels">Belts aand Buckels</Link>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item>
-                    <Link to="/leathergoods">Leather Goods</Link>
-                  </NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link>
+                  <Link to="/watches">Pre-Owned Watches</Link>
+                </Nav.Link>
+
+                <Nav.Link>
+                  <Link to="/jewlery">Jewlery</Link>
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
           <Switch>
             <Route path="/about" component={About}></Route>
             <Route path="/watches" component={Watches}></Route>
-            <Route path="/diamonds" component={Diamonds}></Route>
             <Route path="/jewlery" component={Jewlery}></Route>
-            <Route path="/beltsandbuckels" component={BeltsAndBuckels}></Route>
-            <Route path="/bracelets" component={Bracelets}></Route>
-            <Route path="/goldlink" component={GoldLink}></Route>
-            <Route
-              path="/partsandaccessories"
-              component={PartsAndAccessories}
-            ></Route>
-            <Route path="/leathergoods" component={LeatherGoods}></Route>
-            <Route path="/products" component={Products}></Route>{" "}
             <Route path="/contact/success" component={FormSuccessful}></Route>
             <Route path="/contact" component={ContactUs}></Route>
             <Route path="/" component={Home}></Route>
---
+++ src/App/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+  background-color: #f5f2e9;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-context: center;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  color: white;
+`;
+
+export const TitleBox = styled.div`
+  background-color: #343a40;
+`;
