87e9c4c3301c6412ad129c5976656fb1990d04ce
Michael Tobias <michaelremitobias@gmail.com>
2021-12-23 22:00:03 -0700

    test

--- src/Home/components/Slogan/index.js
+++ src/Home/components/Slogan/index.js
@@ -4,7 +4,7 @@ export default function SloganBox() {
   return (
     <SloganWrapper>
       <Slogan>
-        Want to make time more valuable? <br></br>The Mundo Group can help
+        Want to make time more valuable? <br></br>The Mundo Group can help!
       </Slogan>
     </SloganWrapper>
   );
