d28a3e01fb8db60a3a4b679556aa8326fea0a6a9
Michael Tobias <michaelremitobias@gmail.com>
2022-01-04 22:53:56 -0700

    fixed responsiveness

--- src/ContactUs/index.js
+++ src/ContactUs/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import WishList from "./components/WishList/index.js";
 import { ContactUsContent as Content } from "./components/Content/index.js";
-
 export default function ContactUs() {
   return (
     <>
--- src/Watches/components/WatchBrands/index.js
+++ src/Watches/components/WatchBrands/index.js
@@ -3,7 +3,7 @@ import { Paper, Card, CardContent, Typography, Grid } from "@mui/material/";
 import { Brands } from "./brandsList";
 export default function WatchBrands() {
   return (
-    <Paper>
+    <Paper elevation={8}>
       <BrandsSection>
         <h2>Some of the brands that we have provided our clients with:</h2>
         <BrandList>
